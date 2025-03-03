4cef1a3a93e3abf38cecc2aa56b03845ec7e8cd8
Pedro Thales Sousa Pereira <pedrothalessp@gmail.com>
2025-03-03 12:41:50 -0300

    resolvendo bugs

--- js/app.js
+++ js/app.js
@@ -5,12 +5,12 @@ let exit = document.getElementById('exit');
 let menu = document.getElementById('side_bar');
 let preco = 0;
 
-// teste
+// adicionar itens e remover a imagem
 let botao = document.querySelector(".add");
 let barraEsquerda = document.querySelector("#sumir")
+let itemAdicionado = false;
 
-// Essa função tem como objtivo abrir e fechar a aba
-
+// abrir e fechar o carrihno
 open.addEventListener('click',() => {
     menu.classList.add('abrir')
 });
@@ -20,41 +20,55 @@ exit.addEventListener('click',() => {
     menu.classList.remove('abrir')
 });
 
+// evento de escuta do itens e remoção da imagem
+botao.addEventListener("click", () =>{
+    tabelaDeItens();
+    removeImage();
 
-// Essa função tem como finalidade adicionar os itens no carrinho
-// resolver o problema de estar pegando apenas um item
+});
 
-adicionar.forEach((item) => {
-item.addEventListener("click", () => {
-        let nome = document.querySelector('.little_name');
-        let precos = document.querySelector('.price');
-        let nomeItem = nome.textContent;
-        let precosItem = precos.textContent;
+// adicionar itens
+function tabelaDeItens() {        
+    let nomes = [...document.querySelectorAll('.little_name')];
+    let precos = [...document.querySelectorAll('.price')];
 
-        const itemTr = document.createElement('tr');
-        const nomeTd = document.createElement('td');
-        const precosTd = document.createElement('td');
-
-        nomeTd.textContent = (nomeItem);
-        precosTd.textContent = (precosItem);
-
-        itemTr.appendChild(nomeTd);
-        itemTr.appendChild(precosTd);
-
-        const itens = document.querySelector('.sacola');
-        itens.appendChild(itemTr);
-
-    })
-})
-
-
-// Essa função tem a finalidade de fazer a imagem da esqueda sumir ao add um item no carrinho.
-botao.addEventListener('click', function (){
-    if (barraEsquerda.style.display === 'flex') {
-        barraEsquerda.style.display = 'none';
-    }
     
-    else {
-        barraEsquerda.style.display = 'flex'    ;
-    }
+    nomes.forEach((nome,index) => {    
+    let nomeItem = nome.textContent;
+    let precosItem = precos[index].textContent;
+    
+    const itemTr = document.createElement('tr');
+    const nomeTd = document.createElement('td');
+    const precosTd = document.createElement('td');
+    
+    nomeTd.textContent = (nomeItem);
+    precosTd.textContent = (precosItem);
+    
+    itemTr.appendChild(nomeTd);
+    itemTr.appendChild(precosTd);
+    
+    const itens = document.querySelector('.sacola');
+    itens.appendChild(itemTr);
+
+    console.log(nomes,precos);
+    
+
 })
+}
+
+//remover a imagem
+function removeImage(){
+    itemAdicionado = true;
+
+    if (itemAdicionado) {
+        barraEsquerda.style.display = 'none'
+    }
+        
+    else {
+        barraEsquerda.style.display = 'flex';
+    }
+}
+
+
+
+

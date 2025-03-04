1ce841bbe9ed032aa2bf7865b617ce0d34391440
Pedro Thales Sousa Pereira <pedrothalessp@gmail.com>
2025-03-03 21:31:35 -0300

    criando a soma

--- js/app.js
+++ js/app.js
@@ -3,8 +3,6 @@ const adicionar = document.querySelectorAll(".add")
 let open = document.getElementById('open');
 let exit = document.getElementById('exit');
 let menu = document.getElementById('side_bar');
-let preco = 0;
-
 // adicionar itens e remover a imagem
 let botao = document.querySelectorAll(".add");
 let barraEsquerda = document.querySelector("#sumir")
@@ -22,17 +20,18 @@ exit.addEventListener('click',() => {
 
 // evento de escuta do itens e remoção da imagem
 
-botao.forEach((btn,index) =>{
+botao.forEach((btn,index,) =>{
 btn.addEventListener("click",()=>{
 
     tabelaDeItens(index);
     removeImage();
+    somarItensDoCarrinho() // ver depois
     })
 })
 
 
 
-// adicionar itens
+// adicionar itens + estilização
 function tabelaDeItens(index) {        
     let nomes = [...document.querySelectorAll('.little_name')];
     let precos = [...document.querySelectorAll('.price')];
@@ -43,7 +42,16 @@ function tabelaDeItens(index) {
     const itemTr = document.createElement('tr');
     const nomeTd = document.createElement('td');
     const precosTd = document.createElement('td');
-    
+    nomeTd.style.border = "1px solid black";
+    itemTr.style.border = "1px solid black";
+    precosTd.style.border = "1px solid black";
+    nomeTd.style.width = "60%"
+    precosTd.style.width = "50%"
+    itemTr.style.width = "50%"
+    nomeTd.style.textAlign = "center"
+    precosTd.style.textAlign  = "center"
+    itemTr.style.textAlign = "center"
+
     nomeTd.textContent = (nomeItem);
     precosTd.textContent = (precosItem);
     
@@ -52,11 +60,6 @@ function tabelaDeItens(index) {
     
     const itens = document.querySelector('.sacola');
     itens.appendChild(itemTr);
-
-    console.log(nomes,precos);
-    
-
-
 }
 
 //remover a imagem
@@ -72,8 +75,24 @@ function removeImage(){
     }
 }
 
-// retornou todos os botões nodeList e com o forEach foi percorrido todos eles, pois o lister escuta apenas um evento por vez.
-console.log(botao);
+// somar produtos do carrinho: teste // ver depois
+function somarItensDoCarrinho(nossoResultado){
+    let somaPrecos = document.querySelectorAll(".price");
+    let preco = 0;
+
+
+    
+    somaPrecos.forEach( (elemento) => {
+        preco += parseFloat (elemento.textContent);
+        return
+    })
+
+    console.log(nossoResultado)
+
+
+}
+
+
 
 
 
